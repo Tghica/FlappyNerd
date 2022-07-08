a80c64658c4109114f304d6744fababd20c1dce9
Tghica <108593193+Tghica@users.noreply.github.com>
2022-07-08 09:25:45 +0300

    v.0.3.2

--- Assets/Scripts/BirdMove.cs
+++ Assets/Scripts/BirdMove.cs
@@ -10,29 +10,35 @@ public class BirdMove : MonoBehaviour
     private float currentSpeed;
     private float timerRotation;
     private float unghi;
+    public bool lost = false;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (lost == false)
         {
-            currentSpeed = initialSpeed;
-        }
-        currentSpeed -= gravity * Time.deltaTime;
-        transform.position += new Vector3(0f, currentSpeed * Time.deltaTime, 0f);
-
-
-        timerRotation += Time.deltaTime;
-        if (timerRotation > limitRotation)
-        {
-            timerRotation = 0f;
-            if (currentSpeed < 0f)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                unghi -= 20f;
-                transform.rotation = Quaternion.Euler(0f, 0f, unghi);
+                currentSpeed = initialSpeed;
+            }
+            currentSpeed -= gravity * Time.deltaTime;
+            transform.position += new Vector3(0f, currentSpeed * Time.deltaTime, 0f);
+
+
+            timerRotation += Time.deltaTime;
+            if (timerRotation > limitRotation)
+            {
+                timerRotation = 0f;
+                if (currentSpeed < 0f)
+                {
+                    unghi -= 20f;
+                    transform.rotation = Quaternion.Euler(0f, 0f, unghi);
+                }
             }
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        lost = true;
+        GetComponent<Rigidbody2D>().gravityScale = 1;
         Debug.Log(1);
         gravity = 0f;
         currentSpeed = 0;
